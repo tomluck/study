443a9076d0c27026c667799ef3fc9560a29ded32
tomluck <tamux.jt@gmail.com>
2017-04-10 14:58:25 +0900

    ToDo application created.

--- dotinstall/backbonejs/js/app.js
+++ dotinstall/backbonejs/js/app.js
@@ -1,56 +1,113 @@
 (function() {
-    
-    // Model
-    
-    var Task = Backbone.Model.extend({
-        defaults: {
-            title: 'do something!',
-            completed: false
+// Model
+var Task = Backbone.Model.extend({
+    defaults: {
+        title: 'do something',
+        completed: false
+    },
+    validate: function(attrs) {
+        if (_.isEmpty(attrs.title)) {
+            return 'title must not be empty';
         }
-    });
-    var task = new Task();
-    
-    // View
-    
-    var TaskView = Backbone.View.extend({
-        tagName: 'li',
-        template: _.template( $('#task-template').html() ),
-        render: function() {
-            var template = this.template( this.model.toJSON() );
-            this.$el.html(template);
-            return this;
-        }
-    });
+    },
+    initialize: function() {
+        this.on('invalid', function(model, error){
+            $('#error').html(error);
+        });
+    }
+});
 
-    // Collection
-    var Tasks = Backbone.Collection.extend({
-        model: Task
-    });
-    
-    var TasksView = Backbone.View.extend({
-        tagName: 'ul',
-        render: function() {
-            this.collection.each(function(task) {
-                var taskView = new TaskView({model: task});
-                this.$el.append(taskView.render().el);
-            }, this);
-            return this;
+// Collection
+var Tasks = Backbone.Collection.extend({model: Task});
+
+// View
+var TaskView = Backbone.View.extend({
+    tagName: 'li', // not exist tag(create new)
+    initialize: function() {
+        this.model.on('destroy', this.remove, this)
+        this.model.on('change', this.render, this)
+    },
+    events:{
+        'click .delete': 'destroy',
+        'click .toggle': 'toggle',
+    },
+    toggle: function() {
+        this.model.set('completed', !this.model.get('completed'));
+    },
+    destroy: function() {
+        if (confirm('are you sure?')) {
+            this.model.destroy();
         }
-    });
-    
-    var tasks = new Tasks([
-        {
-            title: 'task1',
-            completed: true
-        },
-        {
-            title: 'task2'
-        },
-        {
-            title: 'task3'
+    },
+    remove: function() {
+        this.$el.remove();
+    },
+    template: _.template($('#task-template').html()),
+    render: function() {
+        var template = this.template(this.model.toJSON());
+        this.$el.html(template);
+        return this;
+    }
+});
+var TasksView = Backbone.View.extend({
+    tagName: 'ul',
+    initialize: function() {
+        this.collection.on('add', this.addNew, this);
+        this.collection.on('change', this.updateCount, this);
+        this.collection.on('destroy', this.updateCount, this);
+    },
+    addNew: function(task) {
+        var taskView = new TaskView({model: task});
+        this.$el.append(taskView.render().el);
+        $('#title').val('').focus();
+        this.updateCount();
+    },
+    updateCount: function() {
+        var uncompletedTasks = this.collection.filter(function(task) {
+            return !task.get('completed');
+        });
+        $('#count').html(uncompletedTasks.length);
+    },
+    render: function() {
+        this.collection.each(function(task) {
+            var taskView = new TaskView({model: task});
+            this.$el.append(taskView.render().el);
+        }, this);
+        this.updateCount();
+        return this;
+    }
+});
+
+var AddTaskView = Backbone.View.extend({
+    el: '#addTask', // already exist tag
+    events: {
+        'submit': 'submit'
+    },
+    submit: function(e) {
+        e.preventDefault();
+        var task = new Task();
+        if (task.set({title: $('#title').val()}, {validate: true})) {
+            this.collection.add(task);
+            $('#error').empty();
         }
-    ]);
-//    console.log(tasks.toJSON());
-    var tasksView = new TasksView({collection: tasks});
-    $('#tasks').html(tasksView.render().el);
+    }
+});
+
+var tasks = new Tasks([
+    {
+        title: 'task1',
+        completed: true
+    },
+    {
+        title: 'task2'
+    },
+    {
+        title: 'task3'
+    }
+]);
+
+var tasksView = new TasksView({collection: tasks});
+var addTaskView = new AddTaskView({collection: tasks});
+$('#tasks').html(tasksView.render().el);
+
 })();
