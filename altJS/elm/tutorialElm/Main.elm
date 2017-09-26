e20a6d1b71188449944f7f53fb0468d67ea8c341
tomluck <tamux.jt@gmail.com>
2017-09-26 15:11:45 +0900

    added elm tutorial resource sample.

---
+++ altJS/elm/tutorialElm/Main.elm
@@ -0,0 +1,25 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text, program)
+import Messages exposing (Msg)
+import Models exposing (Model, initialModel)
+import Update exposing (update)
+import View exposing (view)
+
+init : ( Model, Cmd Msg)
+init = 
+    ( initialModel, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+-- MAIN
+
+main = 
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
